93aebaee5380df5cf3f1826603571397057f4e27
Celeo <mattboulanger@fastmail.com>
2021-01-25 16:19:17 -0800

    Starting work on TUI

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use anyhow::Result;
-use std::{env, path::Path};
+use std::path::Path;
 
 mod api;
 mod models;
-use api::Api;
+// use api::Api;
 mod cache;
-use cache::Cache;
+// use cache::Cache;
+mod screen;
 
 fn main() -> Result<()> {
     if Path::new(".env").exists() {
         dotenv::dotenv().expect("Could not load from .env file");
     }
     pretty_env_logger::init();
-    let api = Api::new(&env::var("API_KEY")?)?;
-    let mut cache = Cache::new();
-    cache.populate_cache(&api)?;
+
+    // let api = Api::new(&env::var("API_KEY")?)?;
+    // let mut cache = Cache::new();
+    // cache.populate_cache(&api)?;
     // let inventories = api.get_all_inventories()?;
 
-    // TODO
+    screen::run()?;
 
     Ok(())
 }
